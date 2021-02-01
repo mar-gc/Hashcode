c7cc2d42c30e29dd104db8649b8c573f2ae9e390
mar-gc <74357872+mar-gc@users.noreply.github.com>
2021-02-01 15:59:23 +0100

    Tros de programa

--- main.cc
+++ main.cc
@@ -4,29 +4,6 @@
 #include <algorithm>
 
 using namespace std;
-
-struct pizza {
-	int index;
-	bool disponible;
-	vector<string> Ingredients;
-}
-
-
-void lectura(vector<pizza>& v){
-    int n=v.size();
-    for(int i=0; i<n; ++i){
-        v[i].index = i;
-        v[i].disponible = true;
-        int numing;
-        cin >> numing;
-        for(int j=0; j<numing; ++j){
-            string ing;
-            cin >> ing;
-            v[i].Ingredients.pushback(ing);
-        }
-        sort(v[i].Ingredients.begin(),v[i].Ingredients.end());
-    }
-	
 	
 // pre: a i b pizzes valides d'un vector de pizzes
 // post: retorna cert si a te més ingredients que b, o bé si els ingredients de a
